d79fd8d3ff166f993a1d9ed331de701abe722d83
PaulHarden <thepharden@gmail.com>
2021-10-16 21:48:01 -0400

    set up layout and functionality for MainMenu and animated states of buttons, set up LD credits screen in Illustrator and Settings screen with placeholder sliders, etc.

---
+++ HeavenlySin/Assets/HeavenlySin/Scripts/Gameplay/UI/MainMenuManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace HeavenlySin
+{
+    /// <summary>
+    /// This class controls the functionality of buttons and scene switching from the Main Menu
+    /// </summary>
+    public class MainMenuManager : MonoBehaviour
+    {
+        #region Public Fields
+        #endregion
+
+        #region Public Methods
+
+        public void MainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void StartNewGame()
+        {
+            SceneManager.LoadScene("Overworld");
+        }
+
+        public void LoadSavedGame()
+        {
+            //load from save file, see Sprite Knight scripts
+        }
+
+        public void Settings()
+        {
+            SceneManager.LoadScene("Settings");
+        }
+
+        public void Credits()
+        {
+            SceneManager.LoadScene("Credits");
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
