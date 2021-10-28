00f50b28755d34ef0bccce384840515ad6cb2fee
EmmyGames <erickjlatshaw@gmail.com>
2021-10-28 15:09:25 -0400

    Made the rest of the sound scriptable objects

--- HeavenlySin/Assets/HeavenlySin/Scripts/Gameplay/AudioManagement/AudioManager.cs
+++ HeavenlySin/Assets/HeavenlySin/Scripts/Gameplay/AudioManagement/AudioManager.cs
@@ -1,12 +1,25 @@
 using HeavenlySin.Audio;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace HeavenlySin.AudioManagement
 {
+    /// <summary>
+    /// This class will handle all the sound and music in the game.
+    /// When other classes need to play a sound or song, it will send an event that
+    /// this class listens for. It will also handle adjustments to volume and other
+    /// settings.
+    /// </summary>
     public class AudioManager : MonoBehaviour
     {
-        public AudioSource[] sources;
+        #region Public Fields
+        
         public Sound[] sounds;
+        public AudioSource[] sources;
+        
+        #endregion
+        
+        #region Public Methods
         
         public void PlaySound(AudioSource audioSource, AudioClip[] clips, int index)
         {
@@ -18,5 +31,7 @@ namespace HeavenlySin.AudioManagement
             sources[(int)sounds[index].soundType].PlayOneShot(sounds[index].clip);
             Debug.Log("Played sound " + sounds[index].clip.name);
         }
+        
+        #endregion
     }
 }
